e651224752c0622467662ceba3ccc8771be27351
aazzam <amandas.azzam@gmail.com>
2024-05-21 17:39:41 -0800

    Update product array and created empty array for cart

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,5 +1,27 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-const products = [];
+const products = [
+  {
+    name: "Cherry",
+    price: 3,
+    quantity: 0,
+    productId: 001,
+    image: "images/cherry.jpg",
+  },
+  {
+    name: "Orange",
+    price: 6,
+    quantity: 0,
+    productId: 002,
+    image: "images/orange.jpg",
+  },
+  {
+    name: "Strawberry",
+    price: 9,
+    quantity: 0,
+    productId: 003,
+    image: "images/strawberry.jpg",
+  },
+];
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
@@ -17,11 +39,15 @@ const products = [];
 
 /* Declare an empty array named cart to hold the items in the cart */
 
+const cart = [];
+
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
+
+function addProductToCart(productID) {}
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
